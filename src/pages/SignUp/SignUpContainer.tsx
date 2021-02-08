4cd43015d8150f3f4347050c710e5352cdbaecd4
uhuikim <uhui94@naver.com>
2021-02-08 09:34:22 +0900

    feat: signup connect backend

--- src/pages/SignUp/SignUpContainer.tsx
+++ src/pages/SignUp/SignUpContainer.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 import SignUpPresentation from './SignUpPresentation';
+import { gql, useMutation } from '@apollo/client';
+
+const CREATE_ACCOUNT = gql`
+  mutation createAccount($createAccountInput: CreateAccountInput!) {
+    createAccount(input: $createAccountInput) {
+      ok
+      error
+    }
+  }
+`;
 
 const SignUpContainer = () => {
+  const [createAccount, { data }] = useMutation(CREATE_ACCOUNT);
+
   return <SignUpPresentation />;
 };
 
