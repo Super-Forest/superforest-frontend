9e50013b435ede9b4f8f6774c14e7b241914d3fd
leye195 <leye195@naver.com>
2021-02-09 00:04:36 +0900

    feat: mutaion, apollo-tooling test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useReactiveVar } from '@apollo/client';
 import LoggedInRoute from './Routers/LoggedInRoute';
 import LoggedOutRoute from './Routers/LoggedOutRoute';
+import { isLoggedInVar } from 'apollo';
 
 const App: React.FC = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
   return isLoggedIn ? <LoggedInRoute /> : <LoggedOutRoute />;
 };
 
--- src/components/loggedOut/Form.tsx
+++ src/components/loggedOut/Form.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, FormEvent, MouseEvent, useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { css } from '@emotion/react';
 import tw from 'twin.macro';
@@ -6,6 +6,10 @@ import { checkEmailValid } from 'lib/utils';
 import FormInput from 'components/common/FormInput';
 import FormLabel from 'components/common/FormLabel';
 import Button from 'components/common/Button';
+import { useForm } from 'react-hook-form';
+import { gql, useMutation } from '@apollo/client';
+import { loginMutation, loginMutationVariables } from '__generated__/loginMutation';
+import { LoginInPut } from '__generated__/globalTypes';
 
 const form = css`
   display: flex;
@@ -51,52 +55,84 @@ const link = css`
   }
 `;
 
-const Form: React.FC = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [isEmailInValid, setIsEmailInValid] = useState(false);
-  const [isPasswordEmpty, setIsPasswordEmpty] = useState(false);
-
-  const handleEmailOnChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const {
-      target: { value },
-    } = e;
-    setEmail(value);
-    if (checkEmailValid(email)) {
-      setIsEmailInValid(false);
-    } else {
-      setIsEmailInValid(true);
+const LOGIN_MUTATION = gql`
+  mutation loginMutation($loginInput: LoginInPut!) {
+    login(input: $loginInput) {
+      ok
+      error
+      token
     }
-  };
-  const handlePasswordOnChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const {
-      target: { value },
-    } = e;
-    setPassword(value);
-  };
-  const handleSignIn = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  }
+`;
+
+interface IForm {
+  email: string;
+  password: string;
+}
+
+const Form: React.FC = () => {
+  const { register, watch, handleSubmit, errors, getValues } = useForm<IForm>();
+  const [loginMutation, { data }] = useMutation<loginMutation, loginMutationVariables>(
+    LOGIN_MUTATION,
+    {
+      onCompleted: (data: loginMutation) => {
+        const {
+          login: { ok, error, token },
+        } = data;
+        if (ok) {
+          console.log(token);
+        }
+      },
+    },
+  );
+  const handleSignIn = () => {
+    const { email, password } = getValues();
+    //isLoggedInVar(true);
+    loginMutation({
+      variables: {
+        loginInput: {
+          email,
+          password,
+        },
+      },
+    });
   };
 
+  const onInValid = () => {
+    console.log('cant sign in');
+  };
   return (
-    <form css={form} onSubmit={handleSignIn}>
+    <form css={form} onSubmit={handleSubmit(handleSignIn, onInValid)}>
       <FormLabel css={label} htmlFor={'email'} text={'Email'} />
       <FormInput
         css={input}
         name={'email'}
-        onChange={handleEmailOnChange}
         placeholder="email@supertree.co"
+        ref={register({
+          required: 'Email is required',
+          pattern: /^[A-Za-z0-9._%+-]+@supertree.co$/,
+        })}
         type="email"
-        value={email}
       />
+      {errors.email?.message && (
+        <span css={tw`font-bold text-red-600`}>{errors.email?.message}</span>
+      )}
+      {errors.email?.type === 'pattern' && (
+        <span css={tw`font-bold text-red-600`}>Only SuperTree Email is Allowed</span>
+      )}
       <FormLabel css={label} htmlFor={'password'} text={'Password'} />
       <FormInput
         css={input}
-        name={'passowrd'}
-        onChange={handlePasswordOnChange}
+        name={'password'}
+        ref={register({
+          required: 'Password should not be empty',
+        })}
         type="password"
-        value={password}
       />
+      {errors.password?.message && (
+        <span css={tw`font-bold text-red-600`}>{errors.password?.message}</span>
+      )}
+
       <Button css={SignInButton} text={'Sign In'} type="submit" />
       <div css={link}>
         <Link to="/findpassword">Forgot password?</Link>
