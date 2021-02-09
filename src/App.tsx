2c30e38c028fed2b1a2896ad264aaf7ae882e600
leye195 <leye195@naver.com>
2021-02-09 09:27:02 +0900

    feat: reactive variable for check user login

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import { useReactiveVar } from '@apollo/client';
+import { isLoggedInVar } from 'apollo';
+import React from 'react';
 import LoggedInRoute from './Routers/LoggedInRoute';
 import LoggedOutRoute from './Routers/LoggedOutRoute';
 
 const App: React.FC = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
   return isLoggedIn ? <LoggedInRoute /> : <LoggedOutRoute />;
 };
 
