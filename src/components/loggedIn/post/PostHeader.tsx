c7e374ee8aab89f456730f8d6fb99663b0b1e8cd
DanYJ <leye195@naver.com>
2021-02-25 09:38:11 +0900

    feature: post (#13)
    * feat: post markup
    
    * feat: post footer markup

--- src/components/loggedIn/post/PostHeader.tsx
+++ src/components/loggedIn/post/PostHeader.tsx
@@ -1,10 +1,42 @@
 import React from 'react';
+import tw from 'twin.macro';
+
+const container = tw`
+  flex
+  items-center
+  py-4
+`;
+
+const headerImageWrapper = tw`
+  h-40px
+  w-40px
+  mx-5
+`;
+
+const headerInfoWrapper = tw`
+  flex
+  flex-col
+  justify-center
+  text-lg
+`;
+
+const userName = tw`
+  text-2xl
+  font-semibold
+`;
 
 const PostHeader = () => {
   return (
-    <section>
-      <div></div>
-      <div></div>
+    <section css={container}>
+      <div css={headerImageWrapper}>
+        <img src={'https://t1.kakaocdn.net/friends/prod/main_tab/feed/npc_20201228113659.png'} />
+      </div>
+      <div css={headerInfoWrapper}>
+        <p css={userName}>User</p>
+        <div>
+          <span>1 hour(s) ago</span>
+        </div>
+      </div>
     </section>
   );
 };
